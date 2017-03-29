c96c14337fa6f8b862f62f8119cc4aec35a5b05b
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-03-29 02:26:17 -0700

    get comparision

--- app/models/erp/carts/compare_item.rb
+++ app/models/erp/carts/compare_item.rb
@@ -6,5 +6,18 @@ module Erp::Carts
     def self.get_all_items
       self.order("created_at DESC")
     end
+    
+    # get comparision hash
+    def self.get_comparision_hash(compare_items)
+			hash = {}
+			compare_items.each_with_index do |compare_item, index|
+				compare_item.product.get_product_property_array.each do |property|
+					hash[property[:name]] = [] if !hash[property[:name]].present?
+					hash[property[:name]][index] = property[:values]
+				end
+			end
+			hash
+		end
+    
   end
 end
