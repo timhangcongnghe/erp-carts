1e69d12eb8338a55d6588f2c6d0bcd96089a5eef
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-04-05 01:16:04 -0700

    get product price and product name

--- app/models/erp/carts/cart_item.rb
+++ app/models/erp/carts/cart_item.rb
@@ -3,8 +3,16 @@ module Erp::Carts
     belongs_to :product, class_name: "Erp::Products::Product"
     belongs_to :cart
     
+    def product_name
+      product.present? ? product.name : ''
+    end
+    
+    def product_price
+      product.present? ? product.product_price : ''
+    end
+    
     def total_price
-      product.price*quantity
+      product.product_price*quantity
     end
     
     def self.number_items
